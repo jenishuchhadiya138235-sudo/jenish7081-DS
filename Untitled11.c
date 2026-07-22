43a7a09e8a22022a755b52b80b31ea08bcba8ff5
jenishuchhadiya138235-sudo <jenish.uchhadiya138235@marwadiuniversity.ac.in>
2026-07-22 12:50:00 +0530

    Add files via upload

---
+++ Untitled11.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int m1[2][3],m2[3][2],m3[3][3];
+
+    int i, j;
+
+    printf("enter first matrix value: ");
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+             printf("%d",&m1[i][j]);
+        }
+
+    }
+
+    printf("enter secound matrix value: ");
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+             printf("%d",&m1[i][j]);
+        }
+
+    }
+
+
+    printf("enter third matrix value: ");
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+             printf("%d",&m1[i][j]);
+        }
+
+    }
+
+}
+
+
